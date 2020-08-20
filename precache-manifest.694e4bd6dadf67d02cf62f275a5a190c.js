eb3199db9f2f0f785266b1a6319fff2a2938cdaa
Orkid <mrs.emadi@yahoo.com>
2020-08-20 08:27:05 +0430

    Updates

---
+++ precache-manifest.694e4bd6dadf67d02cf62f275a5a190c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440ebbbbc9725cc79ca598f62bb54f58",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6c918a28f3506918c1ee",
+    "url": "/robofriends/static/css/main.e9d9f2a6.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c918a28f3506918c1ee",
+    "url": "/robofriends/static/js/main.0e8fed8e.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "d98c9f0ab5fda93ec5b68b6b9bbcabdd",
+    "url": "/robofriends/static/media/Pop Warner.d98c9f0a.woff"
+  }
+]);
